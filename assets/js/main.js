c19fabe18dde6d7fb60e8ee6e826d6fb6da50d8c
Cumayevomr <cumayevomr8@gmail.com>
2025-06-09 09:23:28 +0400

    home__about generating

--- assets/js/main.js
+++ assets/js/main.js
@@ -36,10 +36,20 @@ const swiperHome = new Swiper('.home__swiper', {
           return '<span class="' + className + '">' + String(index + 1).padStart(2, '0') + "</span>";
         },
     },
+
+    // autoplay: {
+    //     delay: 5000,
+    // }
 });
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
+const bgHeader = () =>{
+    const header = document.getElementById('header')
 
+    this.scrollY >= 50 ? header.classList.add('bg-header')
+                       : header.classList.remove('bg-header')
+}
+window.addEventListener('scroll', bgHeader)
 
 /*=============== SHOW SCROLL UP ===============*/ 
 
