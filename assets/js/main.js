8ccae99bb194720f562baf44cbaaaa12901928d6
Cumayevomr <cumayevomr8@gmail.com>
2025-06-09 09:10:54 +0400

    swiper generating

--- assets/js/main.js
+++ assets/js/main.js
@@ -28,26 +28,14 @@ const linkAction = () =>{
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*=============== SWIPER HOME ===============*/ 
-const swiper = new Swiper('.swiper', {
-  // Optional parameters
-  direction: 'vertical',
-  loop: true,
-
-  // If we need pagination
-  pagination: {
-    el: '.swiper-pagination',
-  },
-
-  // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-
-  // And if we need scrollbar
-  scrollbar: {
-    el: '.swiper-scrollbar',
-  },
+const swiperHome = new Swiper('.home__swiper', {
+     pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+        renderBullet: (index, className) => {
+          return '<span class="' + className + '">' + String(index + 1).padStart(2, '0') + "</span>";
+        },
+    },
 });
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
