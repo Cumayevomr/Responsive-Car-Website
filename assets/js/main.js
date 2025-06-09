65b4368fd0fdc6a79c99313d402d0aacb5f9b91d
Cumayevomr <cumayevomr8@gmail.com>
2025-06-09 20:40:25 +0400

    scroll active link

--- assets/js/main.js
+++ assets/js/main.js
@@ -64,13 +64,13 @@ window.addEventListener('scroll', scrollUp)
 const sections = document.querySelectorAll('section[id]')
 
 const scrollActive = () =>{
-    const scrollDown = window.screenY
+    const scrollDown = window.scrollY
 
     sections.forEach(current =>{
         const sectionHeight = current.offsetHeight,
-              sectionTop = current.offsetTop - 58,
-              sectionId = current.getAttribute('id'),
-              sectionClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+                sectionTop = current.offsetTop - 58,
+                sectionId = current.getAttribute('id'),
+                sectionClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
 
               if(scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight){
                 sectionClass.classList.add('active-link')
